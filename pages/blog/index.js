b86acaf2bcc013ba9c4aa8aa060fd7dfac83cecd
Yuichi Nasukawa <nasu0227@gmail.com>
2022-09-16 01:04:25 +0900

    initial commit

---
+++ pages/blog/index.js
@@ -0,0 +1,12 @@
+import Meta from "components/meta";
+import Container from "components/container";
+import Hero from "components/hero";
+
+export default function Blog() {
+  return (
+    <Container>
+      <Meta pageTitle="ブログ"></Meta>
+      <Hero title="Blog" subtitle="Recent Posts" />
+    </Container>
+  );
+}
---
+++ pages/blog/schedule.js
@@ -0,0 +1,16 @@
+import { client } from "lib/api";
+
+export default function Schedule() {
+  return <h1>記事のタイトル</h1>;
+}
+
+export async function getStaticProps() {
+  const resPromise = client.get({
+    endpoint: "blogs",
+  });
+  console.log(resPromise);
+
+  return {
+    props: {},
+  };
+}
