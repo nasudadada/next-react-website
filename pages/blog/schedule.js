71d1f705e90a761a20c6d8ee2a67ca0f7a818763
Yuichi Nasukawa <nasu0227@gmail.com>
2022-09-16 23:24:06 +0900

    chapter7まで完了

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -1,16 +1,30 @@
-import { client } from "lib/api";
+import { getPostBySlug } from "lib/api";
+import Container from "components/container";
 
-export default function Schedule() {
-  return <h1>記事のタイトル</h1>;
+export default function Schedule({
+  title,
+  publish,
+  content,
+  eyecatch,
+  categories,
+}) {
+  return (
+    <Container>
+      <h1>{title}</h1>
+    </Container>
+  );
 }
 
 export async function getStaticProps() {
-  const resPromise = client.get({
-    endpoint: "blogs",
-  });
-  console.log(resPromise);
-
+  const slug = "schedule";
+  const post = await getPostBySlug(slug);
   return {
-    props: {},
+    props: {
+      title: post.title,
+      publish: post.publishDate,
+      content: post.content,
+      eyecatch: post.eyecatch,
+      categories: post.categories,
+    },
   };
 }
