cff0721f569cad9d7b457653e5bdefc9dae39b50
Yuichi Nasukawa <nasu0227@gmail.com>
2022-09-19 23:55:28 +0900

    Chapter10 Finished

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,6 @@
 import Meta from "components/meta";
 import Container from "components/container";
+import Accordion from "components/accordion";
 import Hero from "components/hero";
 import PostBody from "components/post-body";
 import Contact from "components/contact";
@@ -51,6 +52,23 @@ export default function About() {
             <p>
               今までと違うものを作ることで愛着が湧いてきます。そこで興味を持ったことは小さなことでもいいから取り入れて、良いものを作れるようにしています。小さなヒントから新しいものを生み出すようなモノづくりは、これからも続けていきたいです。
             </p>
+
+            <h2>FAQ</h2>
+            <Accordion heading="プログラミングのポイントについて">
+              <p>
+                プログラミングのポイントは、作りたいものを作ることです。楽しいことから思いつき、目標とゴールを決め、そこに向かってさまざまな課題を設定していきながら、プログラムを作っていきます。
+              </p>
+            </Accordion>
+            <Accordion heading="古代語の解読について">
+              <p>
+                古代語を解読するのに必要なのは、書かれた文字その藻だけです。古代の世界観や思考方法。それらを読み取ってこそ、古代の世界観が理解できてきます。
+              </p>
+            </Accordion>
+            <Accordion heading="公開リポジトリの活用について">
+              <p>
+                公開リポジトリを活用すると、全世界のどこからでもアクセスし、開発者が関連するプロジェクトのタスクを利用することができます。
+              </p>
+            </Accordion>
           </PostBody>
         </TwoColumnMain>
         <TwoColumnSidebar>
